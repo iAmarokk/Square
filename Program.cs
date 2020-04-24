f464d60226eb30b77049b32c2366aeb27f24951f
Александр Небайкин <iamarokk@gmail.com>
2020-04-24 11:53:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            Console.WriteLine(square.Circle(4));
+            Console.WriteLine(square.Circle(7.3));
+            Console.WriteLine(square.IsRectangular(4, 5, 6));
+            Console.WriteLine(square.Triangle(5, 6, 7));
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Square
+{
+    public class Square
+    {
+        //вычисляем площадь круга по радиусу
+        public double Circle(double r)
+        {
+            return (Math.PI * r * r);
+        }
+
+        //вычисляем площадь треугольника по трем сторонам
+        public double Triangle(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        //проверка является ли треугольник прямоугольным
+        public bool IsRectangular(double a, double b, double c)
+        {
+            if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (c * c + b * b == a * a))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }                
+        }
+
+    }
+}
